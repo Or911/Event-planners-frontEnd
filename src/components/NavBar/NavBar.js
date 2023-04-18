5cf8706a8a0ace4c24712cf9aa9a53b0307e6538
or hadad <orhadad19@gmail.com>
2023-04-18 21:18:26 +0300

    log in status

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,16 +2,26 @@ import './NavBar.css'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import SideMenu from './SideMenu'
 import { useNavigate} from 'react-router-dom';
-export default function NavBar(){
+
+export default function NavBar({isLogin , updateLoggedIn}){
     const navigate = useNavigate();
 
+
+    function clickConnection (){
+        if (isLogin){
+            localStorage.clear()
+            updateLoggedIn(false)
+            return
+        }
+        navigate('/login')
+    }
 
     return(
         <div className='navBar'>
             <div className='iconsBar'><SideMenu/></div>  
             <h1 className='headerName'>event planner </h1>
-            <button className='loginBt' onClick={()=>navigate('/login')}>Login</button>
-            <div className='iconsBar'><AccountCircleIcon fontSize='large' /></div>
+            <button className='loginBt' onClick={clickConnection}>{isLogin?'Sgin out':'Login'}</button>
+            <div className='iconsBar'><AccountCircleIcon fontSize='large' /><h3>{isLogin?localStorage.getItem("userName"):null}</h3></div>
     
         </div>
     )
