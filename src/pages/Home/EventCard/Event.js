5be7473c3129c10e66b2487527d2596aeac469d6
or hadad <orhadad19@gmail.com>
2023-04-20 15:02:05 +0300

    add css

--- src/pages/Home/EventCard/Event.js
+++ src/pages/Home/EventCard/Event.js
@@ -1,23 +1,21 @@
 import React from 'react'
 import './Event.css'
 import { useNavigate} from 'react-router-dom';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
 export default function Event({userEvent}) {
   const navigate = useNavigate();
   return (
-    <div className='event'>
         <div className='eventCard-container'onClick={()=>navigate(`/events/${userEvent._id}`)}>
+            <img src={userEvent?.img} alt={userEvent?.name} className='imgEvent'/>
+            <div> {userEvent?.name}  {userEvent?.organizer}</div>
 
-          <span>
-            <img src={userEvent?.img} alt=''/>
-            <div> {userEvent?.name} by {userEvent?.organizer}</div>
-          </span>
+            <div className='evenDetails' > 
+            <KeyboardArrowUpIcon/>
+            <p>{userEvent?.name} </p>
+            <p>{userEvent?.location}<LocationOnIcon /></p></div>
 
-          <span>
-            <div> {userEvent?.date}  {userEvent?.location}</div>
-          </span>
         </div>
-
-    </div>
   )
 }
