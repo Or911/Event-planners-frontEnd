9e186cfdcb195ea5882274f304140ad58d511af9
Almog <almo321@gmail.com>
2023-04-20 12:51:44 +0300

    tickets page and api

--- src/ServerAPI/TicketAPI.js
+++ src/ServerAPI/TicketAPI.js
@@ -5,7 +5,24 @@ function getTickets() {
   return axios.request({
     method: "get",
     url: URL_SERVER + "tickets",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    }
   });
 }
 
-export { getTickets };
+function createTicket(eventId , eventPrice) {
+  return axios.request({
+    method: "post",
+    url: URL_SERVER + "ticket",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+    data: {
+      eventId : eventId,
+      eventPrice : eventPrice
+    },
+  });
+}
+
+export { getTickets , createTicket };
